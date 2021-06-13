3c10d53ec2f65a95864654a20b1e0b26ec9b7d16
Avi Udash <udashavi@gmail.com>
2021-06-13 12:01:51 -0500

    changed color-thief package to colorthief

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import ColorThief from 'color-thief'
+import ColorThief from 'colorthief'
 import { connect } from 'react-redux'
 
 import '../style.scss';
-import { changeColors, fetchCurrentSong} from '../actions'
+import { changeColors, fetchCurrentSong } from '../actions'
 
 class Main extends React.Component {
     constructor(props) {
@@ -22,18 +22,18 @@ class Main extends React.Component {
         const colorsArr = await colorThief.getPalette(this.imgRef.current, 2)
         this.props.changeColors(colorsArr)
     }
-    
-    render(){
-        const {name, artists=[], key, bpm, ugLink, albumArt} = this.props.currentSong
 
-        return(
+    render() {
+        const { name, artists = [], key, bpm, ugLink, albumArt } = this.props.currentSong
+
+        return (
             <div id="main">
                 <div className="left-div">
-                    <img src={albumArt} alt="Album Art" ref={this.imgRef} onLoad={this.handleImgLoad} crossOrigin="anonymous"/>
+                    <img src={albumArt} alt="Album Art" ref={this.imgRef} onLoad={this.handleImgLoad} crossOrigin="anonymous" />
                     <h1> {name} </h1>
                     <p className="artists"> {artists.join(', ')} </p>
                 </div>
-                
+
                 <div className="right-div">
                     <div className="info-section">
                         <h1>{key}</h1>
@@ -48,7 +48,7 @@ class Main extends React.Component {
                     <a href={ugLink} target='_blank' rel="noopener noreferrer">
                         <button type='button' className="ugLink"> Ultimate Guitar </button>
                     </a>
-                    <button type='button' onClick={() => this.props.fetchCurrentSong()} style={{backgroundColor: this.props.colors.secondary}}>Check Now Playing</button>
+                    <button type='button' onClick={() => this.props.fetchCurrentSong()} style={{ backgroundColor: this.props.colors.secondary }}>Check Now Playing</button>
                 </div>
             </div>
         )
